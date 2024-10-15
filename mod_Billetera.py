e9f4c1bdb1fbdf6467e79eae82092a6d851f42d4
Valentin-Juarez <vale12juarez@gmail.com>
2024-10-15 18:41:29 -0300

    Actualizacion: Agregue los self y encapsule las variables de los Atributos

--- mod_Billetera.py
+++ mod_Billetera.py
@@ -1,15 +1,15 @@
+#Creacion de la clase Billetera
 class Billetera:
-    def __init__(id_billetera,id_accion,cantidad_accion,valor_promedio,
+    def __init__(self,id_billetera,id_accion,cantidad_accion,valor_promedio,
                  nombre_accion,cantidad_dinero,total_invertido,ganacia,
                  perdidas,id_operacion):
-        id_billetera = id_billetera
-        id_accion = id_accion
-        cantidad_accion = cantidad_accion
-        valor_promedio = valor_promedio
-        nombre_accion = nombre_accion
-        cantidad_dinero = cantidad_dinero
-        total_invertido = total_invertido
-        ganacia = ganacia
-        perdidas = perdidas
-        id_operacion = id_operacion
-        
+        self.__id_billetera = id_billetera
+        self.__id_accion = id_accion
+        self.__cantidad_accion = cantidad_accion
+        self.__valor_promedio = valor_promedio
+        self.__nombre_accion = nombre_accion
+        self.__cantidad_dinero = cantidad_dinero
+        self.__total_invertido = total_invertido
+        self.__ganacia = ganacia
+        self.__perdidas = perdidas
+        self.__id_operacion = id_operacion
