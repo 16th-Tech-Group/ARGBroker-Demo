8cb91d0950dc064fd8dbb1a016237920f04f6bcb
Valentin-Juarez <vale12juarez@gmail.com>
2024-10-15 17:21:18 -0300

    Definicion de los atributos

---
+++ mod_Billetera.py
@@ -0,0 +1,15 @@
+class Billetera:
+    def __init__(id_billetera,id_accion,cantidad_accion,valor_promedio,
+                 nombre_accion,cantidad_dinero,total_invertido,ganacia,
+                 perdidas,id_operacion):
+        id_billetera = id_billetera
+        id_accion = id_accion
+        cantidad_accion = cantidad_accion
+        valor_promedio = valor_promedio
+        nombre_accion = nombre_accion
+        cantidad_dinero = cantidad_dinero
+        total_invertido = total_invertido
+        ganacia = ganacia
+        perdidas = perdidas
+        id_operacion = id_operacion
+        
