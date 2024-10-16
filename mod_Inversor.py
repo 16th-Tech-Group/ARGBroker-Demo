654c34276de16b7537ed618bbff48fbeaa74e737
Guillermo <giraudoguillermo7@gmail.com>
2024-10-16 20:27:16 -0300

    Clase Inversor

---
+++ mod_Inversor.py
@@ -0,0 +1,27 @@
+#Crear Clase Inversor
+class Inversor():
+    def __init__(self,id_persona_cuit,id_billetera,nombre,id_localidad,ex_politica,id_inversor,calle,numero_calle,
+                 correo_electronico,contraseña):
+        self.id_persona_cuit=id_persona_cuit
+        self.id_billetera=id_billetera
+        self.nombre=nombre
+        self.id_localidad=id_localidad
+        self.ex_politica=ex_politica
+        self.id_inversor=id_inversor
+        self.calle=calle
+        self.numero_calle=numero_calle
+        self.correo_electronico=correo_electronico
+        self.contraseña=contraseña
+
+
+
+
+#Crear Metodos
+    #def alta()
+    #def baja()
+    #def editar()
+    #def comprar()
+    #def vender()
+    #def depositar()
+    #def retirar()
+    #def consulta_billetera()
