28d79b5ba6ef43b0b60a1c19f27f8fda31bc815d
Heyme <heyme.coraspe@gmail.com>
2024-10-22 07:55:34 -0300

    metodo editar_max_diario

--- mod_Accion.py
+++ mod_Accion.py
@@ -24,7 +24,7 @@ class Accion():
     #def edit_max_diario
     #def edit_ultimo_cierre
 
-    #heyme Coraspe
+
     def mostrar_max_diario(self):
         return(f"El monto máximo diario es: {self.max_diario}")
     
@@ -32,9 +32,26 @@ class Accion():
     def editar_max_diario(self,nvo_max_diario):
         self.max_diario=nvo_max_diario
         return(f"Monto mínimo diario actualizado a: {self.min_diario}")
+    
 
+    def mostrar_min_diario(self):
+        return (f"El mínimo diario es: {self.min_diario}")
+    
+#def edit_min_diario
+    def editar_min_diario(self,nvo_min_diario):
+        self.min_diario=nvo_min_diario
+        return(f"Monto mínimo diario actualizado a: {self.min_diario}")
+
+#instancia max    
+accion = Accion(1, "Empresa X", 100, 200, 150, 120, 130, 90, 160, 140)
+
+print(accion.mostrar_min_diario())
+print(accion.editar_min_diario(85))
+print(accion.mostrar_min_diario())
+
+''' #instancia min
 accion = Accion(1, "Empresa X", 100, 200, 150, 120, 130, 90, 160, 140)
 
 print(accion.mostrar_max_diario())
 print(accion.editar_max_diario(133))
-print(accion.mostrar_max_diario())
+print(accion.mostrar_max_diario())''' 
