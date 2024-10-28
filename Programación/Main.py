6ba0724d8e4771cf6e7669462536cacbf5b3425f
Luciano Adolfo Cañas <79887578+CryptoEmpireR4@users.noreply.github.com>
2024-10-28 00:22:45 -0300

    Add files via upload

---
+++ Programación/Main.py
@@ -0,0 +1,17 @@
+from mod_Accion import Accion
+from mod_Billetera import Billetera
+from mod_Inversor import Inversor
+from mod_Tablas_Grales import PerfilInversor,Provincia,Localidad
+from mod_Operacion import Operacion
+from Conexion_MySQL import conectar_mysql
+import datetime
+
+#Menu opciones
+print("Menu Inicio")
+print("Opciones")
+print("1 - Inicial Sesion")
+print("2 - Nuevo Usuario")
+
+#Probar Metodos
+
+
