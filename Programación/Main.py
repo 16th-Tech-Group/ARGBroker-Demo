03d9551d6a34b1a2dbf5d1f4d8b4246d896dd197
Luciano Adolfo Cañas <79887578+CryptoEmpireR4@users.noreply.github.com>
2024-11-04 16:34:11 -0300

    Update Main.py

--- Programación/Main.py
+++ Programación/Main.py
@@ -1,17 +1,34 @@
 from mod_Accion import Accion
 from mod_Billetera import Billetera
 from mod_Inversor import Inversor
-from mod_Tablas_Grales import PerfilInversor,Provincia,Localidad
+from mod_Tablas_Grales import PerfilInversor, Provincia,Localidad, menu_inicio, menu_ingreso
 from mod_Operacion import Operacion
-from Conexion_MySQL import conectar_mysql
+import Conexion_MySQL
 import datetime
+import mysql.connector
+from mysql.connector import errorcode
+from Conexion_MySQL import conectar_mysql
 
-#Menu opciones
-print("Menu Inicio")
-print("Opciones")
-print("1 - Inicial Sesion")
-print("2 - Nuevo Usuario")
+menu_inicio()
+opcion_ingreso=int(input("Elegir Opcion: "))
 
+if opcion_ingreso==2:
+    nuevo_inversor=Inversor("id_persona_cuit","id_billetera","nombre","id_localidad","ex_politica","calle","numero_calle","correo_electronico","contraseña")
+    nuevo_inversor.alta_inversor()
+else:
+    correo_electronico=input("Correo Electronico: ")
+    contraseña=input("Contraseña: ")
+    ingreso=Inversor("id_persona_cuit","id_billetera","nombre","id_localidad","ex_politica","calle","numero_calle",correo_electronico,contraseña)
+    ingreso.loguin_usuario(correo_electronico,contraseña)
+    id=conectar_mysql("SELECT id_persona_cuit FROM inversores WHERE correo_electronico = ('{0}')".format(correo_electronico))
+    id=(id[0][0])
+    menu_ingreso()
+    saldo=(conectar_mysql("SELECT saldo_ars FROM billeteras WHERE id_persona_cuit = ('{0}')".format(id)))
+    saldo=(saldo[0][0])
+    print("-------------------------------------------------") 
+    print("Saldo en Efectivo:   $       "+str(saldo))
+    print("-------------------------------------------------")
+    print("Elegi la Operación a realizar: ") 
 #Probar Metodos
 
 
